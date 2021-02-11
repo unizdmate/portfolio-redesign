39c630951eb6785fdd6acbdf73895cd691bf9443
Mate Krezic <matekrezic@gmail.com>
2021-02-11 20:23:17 +0100

    fixed some responsivness issues

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -2,15 +2,14 @@ import React from "react";
 import styles from "../styles/HomeComponent.module.css";
 import ContactForm from "./ContactForm";
 import Link from "next/link";
+import Image from "next/image";
 
 const HomeComponent = () => {
   return (
     <>
       <div className={styles.lead__img__container}>
         <div className={styles.lead__img__caption}>
-          <span className={styles.border}>
-            Front End Development done right!
-          </span>
+          <div className={styles.border}>Front End Development done right!</div>
         </div>
       </div>
 
@@ -29,22 +28,36 @@ const HomeComponent = () => {
 
       <div className={styles.contact__img__container}>
         <div className={styles.contact__img__caption}>
-          <span className={styles.border}>Feel free to contact me!</span>
+          <div className={styles.border}>Feel free to contact me!</div>
         </div>
       </div>
 
       <section className={styles.contact__section}>
         <div className={styles.txt__container}>
+          <Image
+            src="/more.svg"
+            width={75}
+            height={75}
+            onContextMenu={(e) => e.preventDefault()}
+            onDragStart={(e) => e.preventDefault()}
+          />
           <p>Are you looking for someone reliable?</p>
           <p>Someone ready to work and willing to learn?</p>
           <p>Look no further!</p>
+          <Image
+            src="/more.svg"
+            width={50}
+            height={50}
+            onContextMenu={(e) => e.preventDefault()}
+            onDragStart={(e) => e.preventDefault()}
+          />
         </div>
         <ContactForm />
       </section>
 
       <div className={styles.projects__img__container}>
         <div className={styles.projects__img__caption}>
-          <span className={styles.border}>Check out my previous projects</span>
+          <div className={styles.border}>Check out my previous projects</div>
         </div>
       </div>
 
@@ -61,9 +74,7 @@ const HomeComponent = () => {
 
       <div className={styles.closing__img__container}>
         <div className={styles.closing__img__caption}>
-          <span className={styles.border}>
-            Front End Development done right!
-          </span>
+          <div className={styles.border}>Front End Development done right!</div>
         </div>
       </div>
     </>
