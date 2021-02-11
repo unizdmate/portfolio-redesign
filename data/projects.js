0178950183041c53b9a250490b9a37fe2c94c03c
Mate Krezic <matekrezic@gmail.com>
2021-02-11 21:58:09 +0100

    added projects data

---
+++ data/projects.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    id: 1,
+    employer: "Lemon Mint d.o.o.",
+    client: "Jadrolinija",
+    jobDescription: [
+      "Designing and implementing UI components such as: Icon Button, Toggle Switch and Display Grid",
+      "Implementing React compoment to render all the coastal and island ports",
+    ],
+    techStack: ["React.js", "Apollo Client", "Styled Components"],
+  },
+
+  {
+    id: 2,
+    employer: "Lemon Mint d.o.o.",
+    client: "Noor Takaful - Ethical Insurance",
+    jobDescription: [
+      "Designing and implementing Header and 'double' Footer components",
+      "Designing and implementing PDF output",
+      "Implementing React compoment that takes all the user's data and calculates values od life insurance policy",
+    ],
+    techStack: ["React.js", "Apollo Client", "Styled Components"],
+  },
+
+  {
+    id: 3,
+    employer: "University of Zadar",
+    client: "Final Thesis: Demo Full-Stack Web Application for Dog Shelter",
+    jobDescription: [
+      "Planning, designing and coding entire full-stack web application",
+    ],
+    techStack: [
+      "React.js",
+      "Next.js",
+      "Apollo Client",
+      "Styled Components",
+      "Node.js",
+      "Yoga GraphQL Server",
+      "Prisma",
+    ],
+  },
+  {
+    id: 4,
+    employer: "Personal project",
+    client: "Portfolio & CV App",
+    jobDescription: [
+      "Planning, designing and coding serverless web application",
+    ],
+    techStack: ["React.js", "Next.js", "Email.js"],
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import Head from "next/head";
 import HomeComponent from "../components/HomeComponent";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return <HomeComponent />;
