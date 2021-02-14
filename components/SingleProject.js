5d2a40d634d7ab36fc912ffa009b25a19f82d841
Mate Krezic <matekrezic@gmail.com>
2021-02-14 01:36:43 +0100

    fixed responsivness bugs, added github icon

--- components/SingleProject.js
+++ components/SingleProject.js
@@ -1,4 +1,6 @@
 import styles from "../styles/SingleProject.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const SingleProject = ({ project }) => {
   const { id, employer, client, jobDescription, techStack, url } = project;
@@ -29,8 +31,16 @@ const SingleProject = ({ project }) => {
       </div>
       {url ? (
         <div className={styles.btn__container}>
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            Check out on GitHub
+          <a
+            className={styles.single__project__icon}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>Check out on GitHub</p>
+            <div className={styles.single__project__icon__container}>
+              <FontAwesomeIcon icon={faGithub} />
+            </div>
           </a>
         </div>
       ) : null}
