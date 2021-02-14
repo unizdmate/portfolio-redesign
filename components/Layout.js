7f2bcd1f5a64a611526de76c9a18f634f6578cf1
Mate Krezic <matekrezic@gmail.com>
2021-02-14 02:05:50 +0100

    still fixing deployment css bug

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,13 @@ const Layout = ({ children }) => {
         <title>Mate Krezic | Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="../styles/globals.css" />
+        <link rel="stylesheet" href="../styles/Home.module.css" />
+        <link rel="stylesheet" href="../styles/HomeComponent.module.css" />
+        <link rel="stylesheet" href="../styles/Navbar.module.css" />
+        <link rel="stylesheet" href="../styles/ProjectsList.module.css" />
+        <link rel="stylesheet" href="../styles/SingleProject.module.css" />
+        <link rel="stylesheet" href="../styles/AboutmeComponent.module.css" />
+        <link rel="stylesheet" href="../styles/ContactForm.module.css" />
       </Head>
       <Navbar />
       {children}
