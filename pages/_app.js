3fcd3c7ea313d00d31814bfc522314e2e178ed2d
Mate Krezic <matekrezic@gmail.com>
2021-02-14 02:11:06 +0100

    trying the fix from next js buf report

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,26 @@ import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 
+import Router from "next/router";
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
+
+Router.events.on("routeChangeComplete", routeChange);
+Router.events.on("routeChangeStart", routeChange);
+
 function MyApp({ Component, pageProps, router }) {
   return (
     <Layout>
