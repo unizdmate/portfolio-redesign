9bd295ae3c46eeebbbc2e74ac9cba09429418b6e
Mate Krezic <matekrezic@gmail.com>
2021-03-06 17:04:15 +0100

    working on animation smoothness

--- pages/_app.js
+++ pages/_app.js
@@ -18,24 +18,6 @@ import ProjectsComponent from "./../components/ProjectsComponent";
 //without actually rendering them in _app.js solves the problem of
 //CSS glimmer on page transitions
 
-const routeChange = () => {
-  // Temporary fix to avoid flash of unstyled content
-  // during route transitions. Keep an eye on this
-  // issue and remove this code when resolved:
-  // https://github.com/vercel/next.js/issues/17464
-
-  const tempFix = () => {
-    const allStyleElems = document.querySelectorAll('style[media="x"]');
-    allStyleElems.forEach((elem) => {
-      elem.removeAttribute("media");
-    });
-  };
-  tempFix();
-};
-
-Router.events.on("routeChangeComplete", routeChange);
-Router.events.on("routeChangeStart", routeChange);
-
 function MyApp({ Component, pageProps, router }) {
   return (
     <Layout>
