26e24930731135e3e1e6ea057957385243b65c08
Mate Krezic <matekrezic@gmail.com>
2021-03-07 14:30:04 +0100

    upgrades to styles and alignment

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -3,26 +3,70 @@ import ContactForm from "./ContactForm";
 import styles from "../styles/ContactForm.module.css";
 import { RightArrowAnimatedSVG } from "./../animations/svg";
 import { motion } from "framer-motion";
-import { contactComponentVariants } from "../animations/variants";
+import { contactComponentVariants, btnVariants } from "../animations/variants";
+import {
+  faInstagramSquare,
+  faLinkedin,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ContactComponent = () => {
   return (
     <motion.main
-      className={styles.contact__form__container__large}
+      className={styles.page__wrapper}
       variants={contactComponentVariants}
       initial="initial"
       animate="animate"
     >
-      <section className={styles.contact__txt__container}>
-        <h3>Feel free to contact me!</h3>
-        <p>Are you looking for someone reliable?</p>
-        <p>Someone ready to work and willing to learn?</p>
-        <p>Look no further!</p>
-        <div className={styles.svg__div}>
-          <RightArrowAnimatedSVG />
-        </div>
-      </section>
-      <ContactForm source="large" />
+      <div className={styles.title}>
+        <h1>Feel free to contact me!</h1>
+      </div>
+      <div className={styles.contact__form__container__large}>
+        <section className={styles.contact__txt__container}>
+          <p>Are you looking for someone reliable?</p>
+          <p>Someone ready to work and willing to learn?</p>
+          <p>Look no further!</p>
+          <section className={styles.icons__container}>
+            <motion.a
+              className={styles.single__icon}
+              variants={btnVariants}
+              whileHover="hover"
+              href="https://www.instagram.com/matekrezic/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faInstagramSquare} />
+            </motion.a>
+
+            <motion.a
+              className={styles.single__icon}
+              variants={btnVariants}
+              whileHover="hover"
+              href="https://twitter.com/MateKrezic"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faTwitterSquare} />
+            </motion.a>
+
+            <motion.a
+              className={styles.single__icon}
+              variants={btnVariants}
+              whileHover="hover"
+              href="https://hr.linkedin.com/in/mate-krezi%C4%87-711731140"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </motion.a>
+          </section>
+          <div className={styles.svg__div}>
+            <RightArrowAnimatedSVG />
+          </div>
+        </section>
+        <ContactForm source="large" />
+      </div>
     </motion.main>
   );
 };
