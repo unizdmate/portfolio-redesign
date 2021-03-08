b4787fade5163bbbdf8abf60396f8fa29812a442
Mate Krezic <matekrezic@gmail.com>
2021-03-08 17:16:20 +0100

    testing some SEO

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,14 @@ const Layout = ({ children }) => {
       <Head>
         <title>Mate Krezic | Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="https://matekrezic.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Mate Krezic | Portfolio" />
+        <meta property="og:description" content="Mate Krezic" />
+        <meta
+          property="og:image"
+          content="http://localhost:3000/_next/image?url=%2FtestImg.jpg"
+        />
       </Head>
       <Navbar />
       {children}
